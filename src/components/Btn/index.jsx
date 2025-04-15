4f29646ae179117e6b4eb8cbd21be2a1bc023f02
Marina Filippova <marisha.mi0@ya.ru>
2025-04-15 13:11:43 +0300

    Updates

--- src/components/Btn/index.jsx
+++
@@ -1,8 +0,0 @@
-import clsx from "clsx";
-import "./style.css";
-
-const Btn = ({ title, className, onClick }) => {
-  return <button className={clsx("btn", className || "")} onClick={onClick}>{title}</button>;
-};
-
-export default Btn;
--- src/components/Footer/index.jsx
+++
@@ -1,7 +0,0 @@
-import "./style.css";
-
-const Footer = () => {
-  return <footer className="footer">© Revo Coffee Việt Nam - 2018</footer>;
-};
-
-export default Footer;
